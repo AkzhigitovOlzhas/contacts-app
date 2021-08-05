7d327aefa804bcd99fb035ac5df3dd9607e440a2
Akzhigitov Olzhas <willdizzel02@gmail.com>
2021-08-05 15:34:54 +0600

    update

--- src/contacts/Header.jsx
+++ src/contacts/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { Flex, Text } from "rebass";
+import { Flex, Link } from "rebass";
 
 function Header() {
   return (
     <Flex px={3} color="white" bg="black" alignItems="center">
-      <Text
+      <Link
         p={3}
         fontWeight="bold"
         fontSize={4}
-        style={{ display: "flex", alignItems: "center" }}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          cursor: "pointer",
+          textDecoration: "none",
+          color: "#fff",
+        }}
+        href="/"
       >
         <img
           src="./img/icons/Group.png"
@@ -16,7 +23,7 @@ function Header() {
           style={{ width: "30px", marginRight: "6px" }}
         />
         MyContacts
-      </Text>
+      </Link>
     </Flex>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
